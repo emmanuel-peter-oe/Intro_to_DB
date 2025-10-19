f26ee5dacad821d07588e977f4e0f030f2110d86
Emmanuel <emmanuel.peter.oe@gmail.com>
2025-10-19 22:20:46 +0100

    Add all tables for alx_book_store database

--- task_2.sql
+++ task_2.sql
@@ -1,14 +1,11 @@
--- Create and use the database
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
--- Create AUTHORS table
 CREATE TABLE IF NOT EXISTS AUTHORS (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
--- Create BOOKS table
 CREATE TABLE IF NOT EXISTS BOOKS (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130),
@@ -18,7 +15,6 @@ CREATE TABLE IF NOT EXISTS BOOKS (
     FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
 );
 
--- Create CUSTOMERS table
 CREATE TABLE IF NOT EXISTS CUSTOMERS (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
@@ -26,7 +22,6 @@ CREATE TABLE IF NOT EXISTS CUSTOMERS (
     address TEXT
 );
 
--- Create ORDERS table
 CREATE TABLE IF NOT EXISTS ORDERS (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -34,7 +29,6 @@ CREATE TABLE IF NOT EXISTS ORDERS (
     FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
 );
 
--- Create ORDER_DETAILS table
 CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
