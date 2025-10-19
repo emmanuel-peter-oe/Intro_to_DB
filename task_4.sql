66c3c9b0fc1dd4b284f639d43af16c2add73eac7
Emmanuel <emmanuel.peter.oe@gmail.com>
2025-10-19 22:54:15 +0100

    Add task_4.sql to show full description of books table

--- task_4.sql
+++ task_4.sql
@@ -3,6 +3,7 @@ USE alx_book_store;
 SELECT COLUMN_NAME, COLUMN_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+  AND TABLE_NAME = 'Books';
+
 
 
